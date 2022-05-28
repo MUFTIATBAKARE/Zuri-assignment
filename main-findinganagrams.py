5ba012fb4516d315fd187f1549f182fc9fd0aa94
MUFTIATBAKARE <105362326+MUFTIATBAKARE@users.noreply.github.com>
2022-05-28 15:38:50 -0700

    Add files via upload

---
+++ main-findinganagrams.py
@@ -0,0 +1,20 @@
+
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+from pickletools import string1
+
+
+def find_anagram(word, anagram):
+   # [assignment] Add your code here
+    str1 = "word"
+    str2 = "anagram"
+    if (sorted(str1)) == sorted((str2)):
+       return False
+    else:
+     return True
+print(find_anagram("str1", "str2"))
+
+
